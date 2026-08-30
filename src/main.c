c4263b087a7daec968829084b6ca2240767b54f2
Kurowyn <novathesecond802@gmail.com>
2026-08-30 20:25:28 +0100

    added IniEntry type

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+typedef struct IniEntry {
+    char *section;
+    char *key;
+    char *value;
+} IniEntry;
+
+// Our pipeline is basically the following:
+// - An .ini file 
+// - Our parser, which generates an array of IniEntry.
+
 int main(void) {
     puts("Hello world! (Not using printf, because it's just a single string argument.");
     return 0;
